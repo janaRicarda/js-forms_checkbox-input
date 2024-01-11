5f6c22c2f8300b08863f9a4d42b935657a52ec9d
Jana May <jana.ricarda.may@gmail.com>
2024-01-11 12:44:54 +0100

    solve challenge

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -16,9 +17,25 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  if (!tosCheckbox.checked) {
+    showTosError();
+    hideSuccesMessage();
+  } else if (tosCheckbox.checked) {
+    hideTosError();
+    showSuccessMessage();
+  }
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  else {
+    alert("Form submitted");
+  }
 });
+
+function hideSuccesMessage() {
+  successMessage.setAttribute("hidden", "");
+}
+
+function showSuccessMessage() {
+  successMessage.removeAttribute("hidden");
+}
